b1b6bd5d5ff55518d32f32276c30104a83a90b99
mike stedman <github@ravuya.com>
2012-03-28 21:38:44 -0600

    getting kind of crazy with concentric circles

--- patterns.js
+++ patterns.js
@@ -5,9 +5,14 @@ function main() {
 	
 	hose.clear();
 	
-	var originX = 10;
-	var originY = 50;
+	blitConcentricCircles(10, 50, hose);
+	blitConcentricCircles(100, 300, hose);
+	blitConcentricCircles(450, 500, hose);
 	
+	hose.flip();
+}
+
+function blitConcentricCircles(originX, originY, hose) {
 	for(var y = 0; y < canvas.height; ++y) {
 		for(var x = 0; x < canvas.width; ++x) {
 			var colour = { r: 255, g: 255, b: 255, a: 255 };
@@ -15,29 +20,8 @@ function main() {
 			if(Math.round(hose.distance(originX, originY, x, y)) % 10 == 0) {
 				colour.g = 0;
 				colour.b = 0;
-			}
-			
-			hose.draw(x, y, colour);
-		}
-	}
-	
-	blitInversionRect(25, 25, 150, 205, hose);
-	// interference is wacky.
-	blitInversionRect(50, 50, 150, 205, hose);
-	blitInversionRect(75, 75, 150, 205, hose);
-	blitInversionRect(100, 100, 150, 205, hose);
-	
-	hose.flip();
-}
-
-function blitInversionRect(startX, startY, w, h, hose) {
-	for(var y = startY; y < startY + h; ++y) {
-		for(var x = startX; x < startX + w; ++x) {
-			var colour = hose.get(x, y);
-			if((x + y) % 10 == 0) {
-				colour.r = (255 - colour.r);
 				hose.draw(x, y, colour);
-			}
+			}		
 		}
 	}
 }
