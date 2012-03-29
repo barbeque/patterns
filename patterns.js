ce3999d1c939b607f910043b446a6a84d4c04c0d
mike stedman <github@ravuya.com>
2012-03-28 21:54:19 -0600

    This iteration makes way more sense

--- patterns.js
+++ patterns.js
@@ -12,12 +12,13 @@ function main() {
 	drawPolar(canvas.width / 2, canvas.height / 2, function(theta) { return 2 * Math.sin(4 * theta); }, 45.0, hose);
 	drawPolar(canvas.width / 2, canvas.height / 2, function(theta) { return 2 * Math.sin(4 * theta - 4); }, 45.0, hose);
 	drawPolar(canvas.width / 2, canvas.height / 2, function(theta) { return 2 * Math.sin(4 * theta - 12); }, 45.0, hose);
+	drawPolar(canvas.width / 2, canvas.height / 2, function(theta) { return 2 * Math.sin(4 * theta - 4); }, 65.0, hose);
 	
 	hose.flip();
 }
 
 function drawPolar(originX, originY, polarFunction, scale, hose) {
-	for(var theta = 0.0; theta < Math.PI * 2.0; theta += 0.01) {
+	for(var theta = 0.0; theta < Math.PI * 2.0; theta += 0.001) {
 		var r = polarFunction(theta) * scale;
 		var coords = phMath.polarToCartesian(r, theta);
 		
