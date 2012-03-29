e86b116e47aa9bfd9c8b1dbef892c7acb1581b20
mike stedman <github@ravuya.com>
2012-03-28 21:25:03 -0600

    A really interesting pattern has emerged

--- voronoi.js
+++ voronoi.js
@@ -8,7 +8,7 @@ function main() {
 	for(var y = 0; y < canvas.height; ++y) {
 		for(var x = 0; x < canvas.width; ++x) {
 			var colour = { r: 255, g: 255, b: 255, a: 255 };
-			if((x + y) % 4 == 0) {
+			if((x + y) % 5 == 0) {
 				colour.g = 0;
 				colour.b = 0;
 			}
@@ -25,7 +25,7 @@ function blitInversionRect(startX, startY, w, h, hose) {
 	for(var y = startY; y < startY + h; ++y) {
 		for(var x = startX; x < startX + w; ++x) {
 			var colour = hose.get(x, y);
-			if((x + y) % 2 == 0) {
+			if((x * y) % 20 == 0) {
 				colour.r = 255 - colour.r;
 				hose.draw(x, y, colour);
 			}
