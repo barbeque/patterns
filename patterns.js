5b347e90bbd826952a81100b60e93353b800883c
Mike <github@ravuya.com>
2012-05-18 17:08:40 +0000

    Really ugly texture map generation

--- patterns.js
+++ patterns.js
@@ -5,14 +5,50 @@ function main() {
 
 	var state = { offset: { u: 0, v: 0 } };
 
+	var texMap = buildTextureMap();
+	state.texture = texMap;
+
 	var s = function() { step(canvas, context, hose, state); }
 	state.stepFunction = s;
 	setTimeout(s, 1000 / 30);
 }
 
+function buildTextureMap() {
+	var w = 64;
+	var h = 64;
+
+	var checkerboardStep = 8;
+	var yChecked = false;
+	var xChecked = true;
+
+	var map = new Array(h);
+	for(var y = 0; y < map.length; ++y) {
+		map[y] = new Array(w);
+	}
+
+	for(var y = 0; y < h; ++y) {
+		for(var x = 0; x < w; ++x) {
+			if(x > w / 2 && y > h / 2) {
+				map[y][x] = { r: 0, g: 255, b: 0, a: 255 };
+			}
+			else if(y > h / 2) {
+				map[y][x] = { r: 0, g: 128, b: 0, a: 255 };
+			}
+			else if(x > w / 2) {
+				map[y][x] = { r: 128, g: 0, b: 0, a: 255 };
+			}
+			else {
+				map[y][x] = { r: 255, g: 0, b: 0, a: 255 };
+			}
+		}
+	}
+
+	return map;
+}
+
 function step(canvas, context, hose, state) {
-	state.offset.u += 0.05;
-	state.offset.v += 0.05;
+	state.offset.u += 5;
+	state.offset.v += 5;
 
 	hose.clear();
 
@@ -29,10 +65,19 @@ function step(canvas, context, hose, state) {
 			if(distance(x, y, centreX, centreY) < radius) {
 				// For now, just do some stripes
 				var uv = computeUV(x, y, centreX, centreY, radius);
-				var colour = sample(uv, state.offset);
+				var colour = sample(uv, state.offset, state.texture);
 
 				hose.draw(x, y, colour);
 			}
+		}
+	}
+
+	// draw the texture map in the lower right corner
+	for(var y = 0; y < state.texture.length; ++y) {
+		for(var x = 0; x < state.texture[y].length; ++x) {
+			var mapOriginX = 0;
+			var mapOriginY = canvas.height - state.texture.length;
+			hose.draw(x + mapOriginX, y + mapOriginY, state.texture[y][x]);
 		}
 	}
 
@@ -41,12 +86,12 @@ function step(canvas, context, hose, state) {
 	setTimeout(state.stepFunction, 1000 / 30);
 }
 
-function sample(uv, offset) {
-	var r = ((uv.u + offset.u) % 1.0) * 255;
-	var g = ((uv.v + offset.v) % 1.0) * 165;
-	var b = 0;
-	var a = 255;
-	return { r:r, g:g, b:b, a:a };
+function sample(uv, offset, texture) {
+	var yIndex = Math.floor(uv.v * (texture.length - 1)) % texture.length - 1;
+	var xIndex = Math.floor(uv.u * (texture[yIndex].length - 1)) % texture[yIndex].length - 1;
+
+	var c = texture[yIndex][xIndex];
+	return c;
 }
 
 /// Get Euclidian distance between two points on the same coordinate
